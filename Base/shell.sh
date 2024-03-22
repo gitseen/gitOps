bf351e6a1b18004a7755e641b6682429ed6662b6
gitseen <361750221@qq.com>
2024-03-22 11:25:27 +0800

    add + shell-log-info

--- Base/shell.sh
+++ Base/shell.sh
@@ -63,4 +63,36 @@ WARNING "BB"
 
 
 
+color () {
+    RES_COL=60
+    MOVE_TO_COL="echo -en \\033[${RES_COL}G"
+    SETCOLOR_SUCCESS="echo -en \\033[1;32m"
+    SETCOLOR_FAILURE="echo -en \\033[1;31m"
+    SETCOLOR_WARNING="echo -en \\033[1;33m"
+    SETCOLOR_NORMAL="echo -en \E[0m"
+    echo -n "$1" && $MOVE_TO_COL
+    echo -n "["
+    if [ $2 = "success" -o $2 = "0" ] ;then
+        ${SETCOLOR_SUCCESS}
+        echo -n $"  OK  "   
+    elif [ $2 = "failure" -o $2 = "1"  ] ;then
+        ${SETCOLOR_FAILURE}
+        echo -n $"FAILED"
+    else
+        ${SETCOLOR_WARNING}
+        echo -n $"WARNING"
+    fi
+    ${SETCOLOR_NORMAL}
+    echo -n "]"
+    echo
+}
+ 
+check () {
+    if [ $ID = 'ubuntu' -a ${VERSION_ID} = "20.04"  ];then
+        true
+    else
+        color "不支持此操作系统，退出!" 1
+        exit
+    fi
+}
 ```
